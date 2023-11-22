b5bcc18b038b8cf63a01f28e6b8977181952edb2
pink <haibarapink@gmail.com>
2023-11-22 17:07:38 +0800

    实现了大部分常见排序算法

--- sortion.cc
+++ sortion.cc
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cassert>
+#include <cstdio>
 #include <iostream>
+#include <iterator>
 #include <stdlib.h>
 #include <vector>
 
@@ -22,11 +24,11 @@ template <> void print_ln<vector<int>>(const vector<int> &v) {
     assert(false);                                                             \
   }
 
-// sortion : specific operator of 'operator() (vector<int>& data)'
+// sortion : revoke specific operator: 'void operator() (vector<int>& data)'
 
 template <typename Sortion> void check_ok(std::string name) {
   vector<int> data;
-  for (auto i = 0; i < 1024; ++i) {
+  for (auto i = 0; i < 16; ++i) {
     data.push_back(rand());
   }
 
@@ -61,4 +63,98 @@ struct insert_sortion {
   }
 };
 
-int main(int, char **) { check_ok<insert_sortion>("insert_sortion"); }
+struct select_sortion {
+  void operator()(vector<int> &data) {
+    size_t n = data.size();
+    for (auto sorted = 0; sorted < n; ++sorted) {
+      size_t min_in_unsorted = sorted;
+      int min_num = data[min_in_unsorted];
+      for (auto j = sorted; j < n; ++j) {
+        if (min_num > data[j]) {
+          min_in_unsorted = j;
+          min_num = data[min_in_unsorted];
+        }
+      }
+
+      swap(data[min_in_unsorted], data[sorted]);
+    }
+  }
+};
+
+struct bubble_sortion {
+  void operator()(vector<int> &data) {
+    size_t n = data.size();
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = 0; j < n - i - 1; ++j) {
+        if (data[j] > data[j + 1]) {
+          swap(data[j], data[j + 1]);
+        }
+      }
+    }
+  }
+};
+
+struct shell_sortion {
+  void operator()(vector<int> &data) {
+    size_t n = data.size();
+    for (auto gap = n / 2; gap > 0; gap /= 2) {
+      for (auto i = gap; i < n; ++i) {
+        int tmp = data[i];
+        for (auto j = i; j >= gap && data[j] < data[j - gap]; j -= gap) {
+          swap(data[j], data[j - gap]);
+        }
+      }
+    }
+  }
+};
+
+// build heap in origin array
+struct heap_sortion {
+  void operator()(vector<int> &data) {
+    size_t n = data.size();
+    for (auto heap_len = 1; heap_len < n - 1; ++heap_len) {
+      size_t len = heap_len + 1;
+      // heap insertion
+      while (true) {
+        if (data[(len - 1) / 2] > data[len]) {
+          swap(data[(len - 1) / 2], data[len]);
+        }
+        if ((len - 1) / 2 == 0)
+          break;
+        len = (len - 1) / 2;
+      }
+    }
+  }
+};
+
+struct quick_sortion {
+  void inner(vector<int> &d, size_t l, size_t r) {
+    if (l >= r)
+      return;
+    int m = d[l];
+    int p = l;
+    for (auto i = p + 1; i < r; ++i) {
+      if (d[i] < m) {
+        swap(d[++p], d[i]);
+      }
+    }
+    swap(d[l], d[p]);
+    inner(d, l, p);
+    inner(d, p + 1, r);
+  }
+
+  void operator()(vector<int> &data) {
+    size_t n = data.size();
+    inner(data, 0, n);
+  }
+};
+
+int main(int, char **) {
+  check_ok<insert_sortion>("insert_sortion");
+  check_ok<select_sortion>("select_sortion");
+  check_ok<bubble_sortion>("bubble_sortion");
+  check_ok<shell_sortion>("shell_sortion");
+  check_ok<quick_sortion>("quick_sortion");
+  // check_ok<heap_sortion>("heap_sortion"); this is a heap...
+  //  todo write check heap
+}
